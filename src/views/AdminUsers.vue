132ffc37fbebe4907cd318dd62da116aea8ea184
Kagiso Mphayi <91262652+Kagisomphayi@users.noreply.github.com>
2022-03-22 22:12:27 +0200

    Add files via upload

--- src/views/AdminUsers.vue
+++ src/views/AdminUsers.vue
@@ -1,36 +1,42 @@
 <template>
-    <div class="price-title">
-      <div class="container text-center">
-        <ul class="">
-          <a class="nav-link  text-truncate p-0 m-2"
-            ><router-link class="text-white" to="/admin/services">Services</router-link></a
-          >
+  <div class="price-title">
+    <div class="container text-center">
+      <ul class="">
+        <a class="nav-link text-truncate p-0 m-2"
+          ><router-link class="text-white" to="/admin/services"
+            >Services</router-link
+          ></a
+        >
 
-          <a class="nav-link text-truncate p-0 m-2"
-            ><router-link class="text-white" to="/admin/users">Users</router-link></a
-          >
-        </ul>
-      </div>
+        <a class="nav-link text-truncate p-0 m-2"
+          ><router-link class="text-white" to="/admin/users"
+            >Users</router-link
+          ></a
+        >
+      </ul>
     </div>
-<div class="table-content">
+  </div>
+  <div class="table-content">
     <div class="container">
       <table>
-  <thead>
-    <tr>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Job Title</th>
-      <th>Twitter</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td data-column="First Name">James</td>
-      <td data-column="Last Name">Matman</td>
-      <td data-column="Job Title">Chief Sandwich Eater</td>
-      <td data-column="Twitter">@james</td>
-    </tr>
-    <tr>
+        <thead>
+          <tr>
+            <th>user</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Job Title</th>
+            <th>Twitter</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="service in services" :key="service.index">
+            <td data-column="First Name">{{service.index}}</td>
+            <td data-column="First Name">{{service.First_name}}</td>
+            <td data-column="Last Name">{{service.Last_name}}</td>
+            <td data-column="Job Title">{{service.Job_title}}</td>
+            <td data-column="Twitter">{{service.Twitter}}</td>
+          </tr>
+          <!-- <tr>
       <td data-column="First Name">Andor</td>
       <td data-column="Last Name">Nagy</td>
       <td data-column="Job Title">Designer</td>
@@ -53,19 +59,35 @@
       <td data-column="Last Name">Nagy</td>
       <td data-column="Job Title">Chief Sandwich Eater</td>
       <td data-column="Twitter">@szabi</td>
-    </tr>
-  </tbody>
-</table>   
+    </tr> -->
+        </tbody>
+      </table>
     </div>
-  
-</div>
- 
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      services: [
+        {
+          
+          First_name: "Kagiso",
+          Last_name: "Mphayi",
+          Job_title: "Web Dev",
+          Twitter: "Kagiii",
+        },
+        {
+          First_name: "will",
+          Last_name: "Mphayuuuu",
+          Job_title: "Web Deveee",
+          Twitter: "Kagiidddi",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
@@ -75,8 +97,8 @@ a {
 .nav-link {
   display: inline;
 }
-.table-content{
-    padding-top: 200px;
+.table-content {
+  padding-top: 200px;
 }
 .price-title {
   height: 40px;
@@ -85,85 +107,90 @@ a {
   z-index: 100;
   position: fixed;
   top: 110px;
-  
 }
-table { 
-	width: 100%; 
-	border-collapse: collapse; 
-	margin:50px auto;
-	}
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin: 50px auto;
+}
 
 /* Zebra striping */
-tr:nth-of-type(odd) { 
-	background: #eee; 
-	}
+tr:nth-of-type(odd) {
+  background: #eee;
+}
 
-th { 
-	background: #3498db; 
-	color: white; 
-	font-weight: bold; 
-	}
+th {
+  background: #3498db;
+  color: white;
+  font-weight: bold;
+}
 
-td, th { 
-	padding: 10px; 
-	border: 1px solid #ccc; 
-	text-align: left; 
-	font-size: 18px;
-	}
+td,
+th {
+  padding: 10px;
+  border: 1px solid #ccc;
+  text-align: left;
+  font-size: 18px;
+}
 
 /* 
 Max width before this PARTICULAR table gets nasty
 This query will take effect for any screen smaller than 760px
 and also iPads specifically.
 */
-@media 
-only screen and (max-width: 760px),
-(min-device-width: 768px) and (max-device-width: 1024px)  {
-    .table-content{
+@media only screen and (max-width: 760px),
+  (min-device-width: 768px) and (max-device-width: 1024px) {
+  .table-content {
     padding-top: 150px;
-}
+  }
 
-	table { 
-	  	width: 100%; 
-	}
+  table {
+    width: 100%;
+  }
 
-	/* Force table to not be like tables anymore */
-	table, thead, tbody, th, td, tr { 
-		display: block; 
-	}
-	
-	/* Hide table headers (but not display: none;, for accessibility) */
-	thead tr { 
-		position: absolute;
-		top: -9999px;
-		left: -9999px;
-	}
-	
-	tr { border: 1px solid #ccc; }
-	
-	td { 
-		/* Behave  like a "row" */
-		border: none;
-		border-bottom: 1px solid #eee; 
-		position: relative;
-		padding-left: 50%; 
-	}
+  /* Force table to not be like tables anymore */
+  table,
+  thead,
+  tbody,
+  th,
+  td,
+  tr {
+    display: block;
+  }
 
-	td:before { 
-		/* Now like a table header */
-		position: absolute;
-		/* Top/left values mimic padding */
-		top: 6px;
-		left: 6px;
-		width: 45%; 
-		padding-right: 10px; 
-		white-space: nowrap;
-		/* Label the data */
-		content: attr(data-column);
+  /* Hide table headers (but not display: none;, for accessibility) */
+  thead tr {
+    position: absolute;
+    top: -9999px;
+    left: -9999px;
+  }
 
-		color: #000;
-		font-weight: bold;
-	}
+  tr {
+    border: 1px solid #ccc;
+  }
 
+  td {
+    /* Behave  like a "row" */
+    border: none;
+    border-bottom: 1px solid #eee;
+    position: relative;
+    padding-left: 50%;
+  }
+
+  td:before {
+    /* Now like a table header */
+    position: absolute;
+    /* Top/left values mimic padding */
+    top: 6px;
+    left: 6px;
+    width: 45%;
+    padding-right: 10px;
+    white-space: nowrap;
+    /* Label the data */
+    content: attr(data-column);
+
+    color: #000;
+    font-weight: bold;
+  }
 }
 </style>
