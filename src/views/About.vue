90c564a382e285beb96819e95485891f2dc3ac5d
Kagiso Mphayi <91262652+kagisowilly@users.noreply.github.com>
2022-03-27 14:05:17 +0200

    Add files via upload

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,9 +7,11 @@
   </div>
 
 </div>
-  <div class="about">
-   
+  <div class="about text-center container">
+    <h3 class="mb-3">Lorem ipsum dolor sit amet.</h3>
+<p class="text-black">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis est facere suscipit blanditiis et sequi, excepturi velit eligendi dolores! Similique quae, a voluptas quidem dolores porro voluptatem laborum harum totam obcaecati laboriosam molestias. Assumenda, voluptates architecto itaque eius adipisci autem aut iusto? Nostrum autem fugit modi maiores quae explicabo, non voluptatem illum sit esse odio magnam inventore deserunt ipsum earum dolorem quam debitis temporibus harum consequuntur porro itaque perferendis qui. Quasi ullam porro vero alias totam assumenda. Magni perspiciatis et corporis, a voluptates mollitia, molestias similique natus deleniti debitis optio, sit id facilis eum dicta dolores ipsam velit. Veritatis sit, pariatur facere nisi, vero perferendis quos sed, expedita fugiat doloremque reprehenderit quasi nam natus eum nostrum nesciunt doloribus? Temporibus in corrupti nihil tenetur. Fugiat veniam voluptate, in iusto dicta sequi autem! Itaque qui debitis officiis repudiandae, aut ipsam fugit porro natus aspernatur mollitia dolor tenetur maxime. Harum eligendi corporis cumque laudantium dolore! Delectus earum aliquam doloribus sapiente ad voluptatum eos facere cupiditate ipsum magni! Omnis, cumque dolorum nam eveniet numquam enim magni quam similique ut natus a neque, aliquam laboriosam veniam nesciunt itaque eum fugit illum, sunt earum. Cum quia minima veritatis eaque ratione fuga, corporis natus tenetur nostrum soluta doloribus consequuntur dolorem voluptatibus mollitia sint magnam unde repellendus amet doloremque aperiam deleniti voluptates! Delectus ratione dignissimos maiores facilis numquam, tempore necessitatibus esse dolorum ad facere exercitationem? Impedit aspernatur, repudiandae iste eaque nostrum, ut fugiat placeat perferendis aperiam tenetur nihil vel reiciendis dicta quas quibusdam! Voluptas totam eos illum magnam fugiat, sequi harum fugit tempora est eum numquam ipsam quaerat! Mollitia, reiciendis eveniet minus porro officiis quas accusamus adipisci, excepturi dolore earum nemo molestiae quia culpa, repellat ea beatae necessitatibus veritatis! Dicta minus mollitia rerum perferendis. Maiores earum quo iusto ratione fugit, minima accusantium nobis eum natus odit dolorem aspernatur doloribus nihil consequatur veritatis eveniet quasi deleniti facilis delectus sunt temporibus voluptates minus fugiat totam? Ipsam dicta doloribus autem exercitationem deleniti blanditiis nam quod, accusamus delectus nulla et nobis sapiente tenetur sit beatae qui cupiditate facere vitae perspiciatis ea totam molestias cum ullam? Ab sunt quidem voluptatum omnis cupiditate aliquam vero ullam nobis necessitatibus harum totam nulla officia at pariatur enim, non sit perferendis, hic quo dolor perspiciatis? Repellat consequatur error magnam ut ratione voluptate provident esse eligendi. Et fugit, quos molestias nihil, autem quibusdam corporis porro impedit voluptatum distinctio obcaecati quis dicta modi tempora temporibus odit saepe necessitatibus, ad totam? In quae molestiae, debitis at dicta magnam iste incidunt doloribus eos tempora est iure, quaerat vero nulla numquam. Accusamus nesciunt quia labore quibusdam illo cum consequatur voluptatem ipsa voluptates, ut in necessitatibus fuga reiciendis cupiditate velit quidem exercitationem, similique, corrupti quisquam eos inventore unde! Quidem quaerat rerum beatae a iusto ex minima enim facere laboriosam aspernatur tempora optio, ipsa adipisci perspiciatis obcaecati doloribus placeat nostrum delectus ipsum odit. Exercitationem itaque, alias nobis laboriosam consequatur accusamus incidunt at laborum quia dolore aspernatur fuga explicabo. Odio architecto eos adipisci tempore, laboriosam iusto, ad nulla perferendis repellendus excepturi aspernatur voluptate harum veniam.</p>
   </div>
+
 </template>
 
 
@@ -19,8 +21,12 @@ height: 40px;
 width: 100%;
 background-color: rgba(139, 102, 96, 0.924);
 position: fixed;
-top: 140px !important;
+top: 110px !important;
 z-index: 500;
 }
-
+.about{
+  margin-top: 200px;
+  margin-bottom: 50px;
+}
+h3{color: rgb(43, 65, 126);}
 </style>
